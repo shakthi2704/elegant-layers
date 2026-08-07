47efd9c1430e9e57bcf2ce04a063b54ef0ea9862
shakthi <shakthi2704@gmail.com>
2026-08-07 10:56:46 +0530

    folder setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button variant="default" size="default">
+        Default Button
+      </Button>
+    </div>
+  );
+}
